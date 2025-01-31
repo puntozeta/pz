29e497212ef7132f022fbbdd259072407c6b37c7
Sócrates López <josesocrateslopez@gmail.com>
2025-01-31 00:53:19 -0600

    audio invitación

--- cumple/santiago/script.js
+++ cumple/santiago/script.js
@@ -1,10 +1,58 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const overlay = document.getElementById("audio-overlay");
+    const mainContent = document.getElementById("cover");
+    const audio = document.getElementById("background-audio");
+    const playButton = document.getElementById("play-audio");
+
+    // Evento para iniciar el audio y ocultar el overlay
+    playButton.addEventListener("click", function () {
+        audio.play().then(() => {
+            overlay.style.display = "none";  // Ocultar el overlay
+            mainContent.classList.remove("hidden"); // Mostrar el contenido principal
+        }).catch(error => {
+            console.error("Autoplay blocked:", error);
+        });
+    });
+});
+
 function openMap() {
-    // Nombre del lugar
     const placeName = "Sweet Land Confiteria";
-
-    // URL de Google Maps con el nombre del lugar
     const url = `https://www.google.com/maps?q=${encodeURIComponent(placeName)}`;
-
-    // Abrir la URL en una nueva pestaña
     window.open(url, '_blank');
 }
+
+function enableMapButton() {
+    const button = document.getElementById('button-map');
+    button.disabled = false
+}
+
+function disableMapButton() {
+    const button = document.getElementById('button-map');
+    button.disabled = true
+}
+
+
+const page2 = document.getElementById('page-2');
+
+
+page2.addEventListener('change', function () {
+    if (this.checked) {
+        enableMapButton()
+    } else {
+        disableMapButton()
+    }
+});
+
+
+const page1 = document.getElementById('page-1');
+
+page1.addEventListener('change', function () {
+    if (this.checked) {
+        disableMapButton()
+    } else {
+        enableMapButton()
+    }
+});
+
+
